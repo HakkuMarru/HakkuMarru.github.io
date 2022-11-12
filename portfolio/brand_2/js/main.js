7a7ecb11d40dcffb6d99e1af673355a1ba4f9117
Danil Gorbunov <43854565+HakkuMarru@users.noreply.github.com>
2022-11-12 18:27:25 +0100

    Add files via upload

---
+++ portfolio/brand_2/js/main.js
@@ -0,0 +1,11 @@
+/*==================== SCROLL REVEAL ANIMATION ====================*/
+const sr = ScrollReveal({
+    origin: 'top',
+    distance: '100px',
+    duration: 2000,
+    delay: 200,
+})
+
+sr.reveal(`.content`)
+
+
